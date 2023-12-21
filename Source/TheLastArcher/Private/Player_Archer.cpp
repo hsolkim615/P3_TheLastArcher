f8c1495982bd5de16a17cad72e271d07e5bd9a9c
hsolkim615 <144770909+hsolkim615@users.noreply.github.com>
2023-12-21 23:14:02 +0900

    231221 hs - player클래스 일부 설정 및 bow, arrow 클래스 생성
    player클래스 일부 설정 및 언리얼 내 input 설정
    
    bow, arrow 클래스 생성

--- Source/TheLastArcher/Private/Player_Archer.cpp
+++ Source/TheLastArcher/Private/Player_Archer.cpp
@@ -5,8 +5,9 @@
 #include "Camera/CameraComponent.h"
 #include "MotionControllerComponent.h"
 #include "Components/SkeletalMeshComponent.h"
-#include <../../../../../../../Plugins/Runtime/XRBase/Source/XRBase/Public/HeadMountedDisplayFunctionLibrary.h>
-#include <../../../../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h>
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 // Sets default values
 APlayer_Archer::APlayer_Archer()
@@ -18,17 +19,19 @@ APlayer_Archer::APlayer_Archer()
 	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
 	CameraComp->SetupAttachment(RootComponent);
 
-	// Hmd 
+	// Hmd - 기기 헤드 
 	HmdMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Hmd Mesh"));
 	HmdMesh->SetupAttachment(CameraComp);
 
 
-	// 오른손
+	// 오른손================
+	// 오른손 컨트롤러
 	RightController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightController"));
 	RightController->SetupAttachment(RootComponent);
 	RightController->SetRelativeLocation(FVector(30, 40, 0));
 	RightController->SetTrackingMotionSource(FName("Right")); // 트래킹에 필요
 
+	// 오른손 외형
 	RightHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("RightHand"));
 	RightHand->SetupAttachment(RightController);
 	RightHand->SetRelativeRotation(FRotator(45, 0, 90));
@@ -38,14 +41,17 @@ APlayer_Archer::APlayer_Archer()
 	if (RightHandMesh.Succeeded()) {
 		RightHand->SetSkeletalMesh(RightHandMesh.Object);
 	}
+	// 오른손================
 
 
-	// 왼손
+	// 왼손==============
+	// 왼손 컨트롤러
 	LeftController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftController"));
 	LeftController->SetupAttachment(RootComponent);
 	LeftController->SetRelativeLocation(FVector(30, -40, 0));
 	LeftController->SetTrackingMotionSource(FName("Left"));// 트래킹에 필요
 
+	// 왼손 외형
 	LeftHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("LeftHand"));
 	LeftHand->SetupAttachment(LeftController);
 	LeftHand->SetRelativeRotation(FRotator(-45, -180, 90));
@@ -55,6 +61,10 @@ APlayer_Archer::APlayer_Archer()
 	if (LeftHandMesh.Succeeded()) {
 		LeftHand->SetSkeletalMesh(LeftHandMesh.Object);
 	}
+	// 왼손==============
+
+
+
 
 }
 
@@ -63,17 +73,20 @@ void APlayer_Archer::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// 트래킹
+	// 트래킹 설정===============
 	UHeadMountedDisplayFunctionLibrary::SetTrackingOrigin(EHMDTrackingOrigin::Stage);
 
 	if (APlayerController* PlayerController = GetWorld()->GetFirstPlayerController()) {
 		UEnhancedInputLocalPlayerSubsystem* subsys = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
 
-		if (subsys != nullptr && imc != nullptr) {
-			subsys->AddMappingContext(imc, 0);
+		if (subsys != nullptr && InputMappingContext != nullptr) {
+			subsys->AddMappingContext(InputMappingContext, 0);
 
 		}
 	}
+	// 트래킹 설정===============
+
+
 
 }
 
@@ -89,5 +102,31 @@ void APlayer_Archer::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// PlayerInputComponent 형변환
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+
+	if (EnhancedInputComponent) {
+		// *****플레이어 조작 키 바인딩 구현
+		
+		// 플레이어 이동
+
+		// 플레이어 시야 이동
+
+		// 플레이어 전방으로 빠른 이동
+
+		// 플레이어 화살 장전
+		
+		// 플레이어 공격 - 활 시위 잡기, 놓기
+		
+		// 플레이어 화살 종류 전환
+		
+		// 플레이어 활 인벤토리 열기/닫기
+
+		// 플레이어 아이템 잡기
+
+
+
+	}
 }
 
