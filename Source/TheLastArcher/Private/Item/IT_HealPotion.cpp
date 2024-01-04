d98dd3206560622f2d69c5c4e3d7dcf39a9e884f
KSE <fplls12@gmail.com>
2024-01-04 22:16:33 +0900

    .

---
+++ Source/TheLastArcher/Private/Item/IT_HealPotion.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/IT_HealPotion.h"
+
---
+++ Source/TheLastArcher/Public/Item/IT_HealPotion.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item_Base.h"
+#include "IT_HealPotion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELASTARCHER_API AIT_HealPotion : public AItem_Base
+{
+	GENERATED_BODY()
+	
+};
